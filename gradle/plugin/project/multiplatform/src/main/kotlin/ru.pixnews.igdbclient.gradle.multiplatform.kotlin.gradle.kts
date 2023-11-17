2601f1e14be765463a5dd59d9eac03588b32a8dc
Alexey Illarionov <alexey@0xdc.ru>
2023-11-18 01:49:00 +0300

    Version updates
    * Bump Kotlin Language/API version to 1.9.0
    * Update Gradle to 8.5-rc-4
    * AGP 8.1.4
    * atomicfu 0.23.1
    * dokka 1.9.10
    * junit-platform-launcher 1.10.1
    * kotlinx-serialization 1.6.1
    * ktor 2.3.6
    * wire 4.9.3

--- gradle/plugin/project/multiplatform/src/main/kotlin/ru.pixnews.igdbclient.gradle.multiplatform.kotlin.gradle.kts
+++ gradle/plugin/project/multiplatform/src/main/kotlin/ru.pixnews.igdbclient.gradle.multiplatform.kotlin.gradle.kts
@@ -20,8 +20,8 @@ kotlin {
     sourceSets {
         all {
             languageSettings {
-                languageVersion = "1.8"
-                apiVersion = "1.8"
+                languageVersion = "1.9"
+                apiVersion = "1.9"
                 listOf(
                     "kotlin.RequiresOptIn",
                     "kotlinx.coroutines.ExperimentalCoroutinesApi",
