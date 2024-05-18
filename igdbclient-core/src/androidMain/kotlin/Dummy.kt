ffb8f4f41666d47a2aa68d54c536fbfc74b7ab0d
Alexey Illarionov <alexey@0xdc.ru>
2024-05-18 16:03:13 +0300

    Remove common package from paths following Kotlin Code Conventions

---
+++ igdbclient-core/src/androidMain/kotlin/Dummy.kt
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) 2024, the Igdbclient project authors and contributors. Please see the AUTHORS file for details.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+ */
+
+@file:Suppress("FILE_CONTAINS_ONLY_COMMENTS", "FILE_NO_BLANK_LINE_BETWEEN_BLOCKS")
+
+// Workaround for https://youtrack.jetbrains.com/issue/KTIJ-27842
+package ru.pixnews.igdbclient
