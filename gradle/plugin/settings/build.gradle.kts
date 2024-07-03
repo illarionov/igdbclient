fac2f933b182933735577ddf2a22841f2b92ca63
Alexey Illarionov <alexey@0xdc.ru>
2024-07-03 14:06:51 +0300

    Remove gradle-enterprise configuration

--- gradle/plugin/settings/build.gradle.kts
+++ gradle/plugin/settings/build.gradle.kts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023, the Igdbclient project authors and contributors. Please see the AUTHORS file for details.
+ * Copyright (c) 2023-2024, the Igdbclient project authors and contributors. Please see the AUTHORS file for details.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
  */
 plugins {
@@ -7,7 +7,3 @@ plugins {
 }
 
 group = "ru.pixnews.igdbclient.gradle.settings"
-
-dependencies {
-    implementation("com.gradle:gradle-enterprise-gradle-plugin:3.17.2")
-}
